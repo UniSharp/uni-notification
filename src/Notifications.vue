5935947522d5bf78b51fef4e9d0eab175662714f
Stream <stream@unisharp.com>
2017-10-11 18:13:20 +0800

    indent with spaces

--- src/Notifications.vue
+++ src/Notifications.vue
@@ -1,37 +1,37 @@
 <template>
-	<div class="notifications">
-		<transition-group name="list" tag="div">
-			<notification v-for="item in items" :item="item" :key="item" @close="removeItem">{{ item }}</notification>
-		</transition-group>
-	</div>
+  <div class="notifications">
+    <transition-group name="list" tag="div">
+      <notification v-for="item in items" :item="item" :key="item" @close="removeItem">{{ item }}</notification>
+    </transition-group>
+  </div>
 </template>
 
 <script>
-	import Notification from './Notification.vue';
+  import Notification from './Notification.vue';
 
-	export default {
-		components: {
-			Notification
-		},
-		data() {
-			return {
-				items : []
-			}
-		},
-		methods: {
-			removeItem(item) {
-				let key = this.items.indexOf(item)
-				if (key >= 0) {
-					this.items.splice(key, 1)
-				}
-			}
-		},
-		created() {
-			this.items = this.$notify.getItems();
-		}
-	}
+  export default {
+    components: {
+      Notification
+    },
+    data() {
+      return {
+        items : []
+      }
+    },
+    methods: {
+      removeItem(item) {
+        let key = this.items.indexOf(item)
+        if (key >= 0) {
+          this.items.splice(key, 1)
+        }
+      }
+    },
+    created() {
+      this.items = this.$notify.getItems();
+    }
+  }
 </script>
 
 <style lang="scss">
-	@import "../assets/style.scss"
+  @import "../assets/style.scss";
 </style>
--- src/index.js
+++ src/index.js
@@ -2,58 +2,58 @@ import Notifications from './Notifications.vue';
 
 const Notify = {
 
-	items: [],
+  items: [],
 
-	options: {
-		duration: 5000
-	},
+  options: {
+    duration: 5000
+  },
 
-	showItem(item) {
-		this.items.push({
-			type: item.type,
-			message: item.message,
-			duration: this.options.duration
-		});
-	},
+  showItem(item) {
+    this.items.push({
+      type: item.type,
+      message: item.message,
+      duration: this.options.duration
+    });
+  },
 
-	install(Vue, options) {
+  install(Vue, options) {
 
-    	Vue.component('notifications', Notifications)
+    Vue.component('notifications', Notifications)
 
-		Vue.prototype.$notify = {
-			show: (item) => {
-				this.showItem(item)
-			},
+    Vue.prototype.$notify = {
+      show: (item) => {
+        this.showItem(item)
+      },
 
-			success: (message) => {
-				this.showItem({ type: 'success', message: message });
-			},
+      success: (message) => {
+        this.showItem({ type: 'success', message: message });
+      },
 
-			danger: (message) => {
-				this.showItem({ type: 'danger', message: message });
-			},
+      danger: (message) => {
+        this.showItem({ type: 'danger', message: message });
+      },
 
-			warning: (message) => {
-				this.showItem({ type: 'warning', message: message });
-			},
+      warning: (message) => {
+        this.showItem({ type: 'warning', message: message });
+      },
 
-			info: (message) => {
-				this.showItem({ type: 'info', message: message });
-			},
+      info: (message) => {
+        this.showItem({ type: 'info', message: message });
+      },
 
-			primary: (message) => {
-				this.showItem({ type: 'primary', message: message });
-			},
+      primary: (message) => {
+        this.showItem({ type: 'primary', message: message });
+      },
 
-			default: (message) => {
-				this.showItem({ type: 'default', message: message });
-			},
+      default: (message) => {
+        this.showItem({ type: 'default', message: message });
+      },
 
-			getItems: () => {
-				return this.items
-			}
-		}
-	}
+      getItems: () => {
+        return this.items
+      }
+    }
+  }
 };
 
 export default Notify;
