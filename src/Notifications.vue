6aa79ff701fe6b0f503ecb8512b06defd3f542c1
Stream <stream@unisharp.com>
2017-12-08 18:21:59 +0800

    add base css for notifications container

--- src/Notifications.vue
+++ src/Notifications.vue
@@ -26,3 +26,20 @@
     }
   }
 </script>
+
+<style scoped>
+  .notifications {
+    position: fixed;
+    top: 0;
+    right: 0;
+    padding: 2rem 2rem 0 2rem;
+    z-index: 999;
+    width: 16rem;
+  }
+
+  @media screen and (max-width: 575px) {
+    .notifications {
+      width: 100%;
+    }
+  }
+</style>
